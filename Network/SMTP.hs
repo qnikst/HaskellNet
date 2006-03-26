216189168fe47017cdb4c1924a6338ed5137efa4
mukai <mukai@jmuk.org>
2006-03-26 09:52:50 -0800

    refine SMTP implementation

--- Network/SMTP.hs
+++ Network/SMTP.hs
@@ -14,10 +14,10 @@
 module Network.SMTP
     ( SMTPMethods(..)
     , SMTPResponse(..)
-    , SMTPConnection
+    , Connect
     , connectSMTPPort
     , connectSMTP
-    , sendMessage
+    , sendCommand
     , closeSMTP
     , sendMail
     , doSMTPPort
@@ -35,80 +35,126 @@ import Data.List
 
 import Prelude hiding (catch)
 
-type SMTPConnection = Socket
+type Connect = Socket
 
-data SMTPMethods = HELO String -- ^ String means a hostname
-                 | MAIL String -- ^ String means the sender name (does not required `from:')
-                 | RCPT String -- ^ String means the receiver name
-                 | DATA String -- ^ String means the mail data
-                 | RSET
-                 | QUIT
+data Command = HELO String
+             | EHLO String
+             | MAIL String
+             | RCPT String
+             | DATA String
+             | EXPN String
+             | VRFY String
+             | HELP String
+             | NOOP
+             | RSET
+             | QUIT
+               deriving Show
 
-data SMTPResponse = Ok
-                  | Quit
-                  | NotImplemented
+type ReplyCode = Int
+
+data Response = Ok
+              | SystemStatus
+              | HelpMessage
+              | ServiceReady
+              | ServiceClosing
+              | UserNotLocal
+              | CannotVerify
+              | StartMailInput
+              | ServiceNotAvailable
+              | MailboxUnavailable
+              | ErrorInProcessing
+              | InsufficientSystemStorage
+              | SyntaxError
+              | ParameterError
+              | CommandNotImplemented
+              | BadSequence
+              | ParameterNotImplemented
+              | MailboxUnavailableError
+              | UserNotLocalError
+              | ExceededStorage
+              | MailboxNotAllowed
+              | TransactionFailed
                     deriving (Show, Eq)
 
-instance Enum SMTPResponse where
-    fromEnum Ok = 250
-    fromEnum Quit = 221
-    fromEnum NotImplemented = 502
-    toEnum 250 = Ok
-    toEnum 221 = Quit
-    toEnum 502 = NotImplemented
-    toEnum _   = undefined
+codeToResponse :: Num a => a -> Response
+codeToResponse 211 = SystemStatus
+codeToResponse 214 = HelpMessage
+codeToResponse 220 = ServiceReady
+codeToResponse 221 = ServiceClosing
+codeToResponse 250 = Ok
+codeToResponse 251 = UserNotLocal
+codeToResponse 252 = CannotVerify
+codeToResponse 354 = StartMailInput
+codeToResponse 421 = ServiceNotAvailable
+codeToResponse 450 = MailboxUnavailable
+codeToResponse 451 = ErrorInProcessing
+codeToResponse 452 = InsufficientSystemStorage
+codeToResponse 500 = SyntaxError
+codeToResponse 501 = ParameterError
+codeToResponse 502 = CommandNotImplemented
+codeToResponse 503 = BadSequence
+codeToResponse 504 = ParameterNotImplemented
+codeToResponse 550 = MailboxUnavailableError
+codeToResponse 551 = UserNotLocalError
+codeToResponse 552 = ExceededStorage
+codeToResponse 553 = MailboxNotAllowed
+codeToResponse 554 = TransactionFailed
 
 crlf = "\r\n"
 
--- | connecting SMTP server with the specified name and port number.
-connectSMTPPort :: Integral a => 
-                   String     -- ^ name of the server
-                -> a          -- ^ port number
-                -> IO SMTPConnection
+connectSMTPPort :: Integral a => String -> a -> IO Connect
 connectSMTPPort hostname port =
     do sock <- socket AF_INET Stream 6
        host <- inet_addr hostname `catch`
                (\_ -> getHostByName hostname >>= return . head . hostAddresses)
        connect sock (SockAddrInet (fromInteger $ toInteger port) host)
-       resp <- recv sock 4096
-       unless (read (takeWhile (/=' ') resp) == 220) $
+       (code, msg) <- parseResponse sock
+       unless (code == 220) $
               do sClose sock
                  fail "cannot connect to server"
-       return sock
+       senderHost <- getHostName
+       (code, msg) <- sendCommand (Connection sock []) (EHLO senderHost)
+       unless (code == 250) $
+              do (code, msg) <- sendCommand (Connection sock [])
+                                  (HELO senderHost)
+                 unless (code == 250) $
+                        do sClose sock
+                           fail "cannot connect to server"
+       return (Connection sock (tail $ lines msg))
 
--- | connecting SMTP server with the specified name and port 25.
-connectSMTP :: String     -- ^ name of the server
-            -> IO SMTPConnection
+connectSMTP :: String -> IO Connect
 connectSMTP = flip connectSMTPPort 25
 
--- | send a method to a server
-sendMessage :: SMTPConnection -> SMTPMethods -> IO (SMTPResponse, String)
+sendMessage :: Connect -> SMTPMethods -> IO (SMTPResponse, String)
 sendMessage sock (DATA dat) =
     do ssize <- send sock $ "DATA\r\n"
        unless (ssize == 6) $ fail "cannot send method DATA"
-       (code, _:msg) <- liftM (span (/=' ')) $ recv sock 4096
-       unless (read code == 354) $ fail "this server cannot accept any data."
+       (code, msg) <- parseResponse sock
+       unless (code == 354) $ fail "this server cannot accept any data."
        mapM_ sendLine $ lines dat ++ ["."]
-       (code, _:msg) <- liftM (span (/=' ')) $ recv sock 4096
-       return (toEnum $ read code, msg)
+       parseResponse sock
     where sendLine l = do ssize <- send sock (l ++ crlf)
                           unless (ssize == length l + 2) $
                                  fail "cannot send data."
-sendMessage sock meth =
-    do ssize <- send sock $ methodToMsg meth
-       unless (ssize == length (methodToMsg meth)) $ fail "cannot send data."
-       (code, _:msg) <- liftM (span (/=' ')) $ recv sock 4096
-       return (toEnum $ read code, msg)
-    where methodToMsg (HELO host)   = "HELO " ++ host ++ crlf
-          methodToMsg (MAIL sender) = "MAIL FROM:" ++ sender ++ crlf
-          methodToMsg (RCPT receiver) = "RCPT TO:" ++ receiver ++ crlf
-          methodToMsg RSET = "RSET\r\n"
-          methodToMsg QUIT = "QUIT\r\n"
+sendCommand (Connection sock _) meth =
+    do ssize <- send sock command
+       unless (ssize == length command) $ fail "cannot send data."
+       parseResponse sock
+    where command = case meth of
+                      (HELO param) -> "HELO " ++ param ++ crlf
+                      (EHLO param) -> "EHLO " ++ param ++ crlf
+                      (MAIL param) -> "MAIL FROM:<" ++ param ++ ">" ++ crlf
+                      (RCPT param) -> "RCPT TO:<" ++ param ++ ">" ++ crlf
+                      (EXPN param) -> "EXPN " ++ param ++ crlf
+                      (VRFY param) -> "VRFY " ++ param ++ crlf
+                      (HELP msg)   -> if null msg
+                                        then "HELP\r\n"
+                                        else "HELP " ++ msg ++ crlf
+                      NOOP         -> "NOOP\r\n"
+                      RSET         -> "RSET\r\n"
+                      QUIT          -> "QUIT\r\n"
 
--- | 
--- close the connection.  This function send the QUIT method, so you
--- do not have to QUIT method explicitly.
-closeSMTP :: SMTPConnection -> IO ()
+closeSMTP :: Connect -> IO ()
 closeSMTP conn = do sendMessage conn QUIT
                     sClose conn
 
@@ -118,15 +164,14 @@ closeSMTP conn = do sendMessage conn QUIT
 sendMail :: String   -- ^ sender mail
          -> [String] -- ^ receivers
          -> String   -- ^ data
-         -> SMTPConnection
+         -> Connect
          -> IO ()
-sendMail sender receivers dat conn = catcher `handle` mainProc
-    where mainProc =  do host <- getHostName
-                         (Ok, _) <- sendMessage conn (HELO host)
-                         (Ok, _) <- sendMessage conn (MAIL sender)
-                         vals <- mapM (sendMessage conn . RCPT) receivers
-                         unless (all ((==Ok) . fst) vals) $ fail "sendMail error"
-                         (Ok, _) <- sendMessage conn (DATA dat)
+sendMail sender receivers dat conn =
+    catcher `handle` mainProc
+    where mainProc =  do (250, _) <- sendCommand conn (MAIL sender)
+                         vals <- mapM (sendCommand conn . RCPT) receivers
+                         unless (all ((==250) . fst) vals) $ fail "sendMail error"
+                         (250, _) <- sendCommand conn (DATA dat)
                          return ()
           catcher e@(PatternMatchFail _) = fail "sendMail error"
           catcher e = throwIO e
@@ -134,12 +179,9 @@ sendMail sender receivers dat conn = catcher `handle` mainProc
 -- | 
 -- doSMTPPort open a connection, and do an IO action with the
 -- connection, and then close it.
-doSMTPPort :: Integral a => String -> a -> (SMTPConnection -> IO b) -> IO b
+doSMTPPort :: Integral a => String -> a -> (Connect -> IO b) -> IO b
 doSMTPPort host port execution =
     bracket (connectSMTPPort host port) closeSMTP execution
 
--- | 
--- doSMTP is the similar to doSMTPPort, except that it does not
--- require port number but connects to the server with port 25.
-doSMTP :: String -> (SMTPConnection -> IO a) -> IO a
+doSMTP :: String -> (Connect -> IO a) -> IO a
 doSMTP host execution = doSMTPPort host 25 execution
