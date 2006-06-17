2c86151642bbd9f345e121f81cc5b0b3f791bde7
mukai <mukai@jmuk.org>
2006-06-16 23:51:34 -0700

    modify SMTP from hazakura impl. and using Network.Stream to HTTP

--- Network/SMTP.hs
+++ Network/SMTP.hs
@@ -12,9 +12,9 @@
 -- 
 
 module Network.SMTP
-    ( SMTPMethods(..)
-    , SMTPResponse(..)
-    , Connect
+    ( Command(..)
+    , Response(..)
+    , SMTPConnection
     , connectSMTPPort
     , connectSMTP
     , sendCommand
@@ -25,21 +25,22 @@ module Network.SMTP
     )
     where
 
+import Network.TCP
+import Network.Stream
 import Network.BSD
-import Network.Socket
 
 import Control.Exception
 import Control.Monad
 
-import Data.List
+import Data.List (intersperse)
 
 import Base64
 
 import Prelude hiding (catch)
 
-type Connect = Socket
+data SMTPConnection = SMTPC !Connection ![String]
 
-data AuthType = PLAIN_AUTH
+data AuthType = PLAIN_AUTH deriving (Show, Eq)
 
 data Command = HELO String
              | EHLO String
@@ -53,7 +54,7 @@ data Command = HELO String
              | NOOP
              | RSET
              | QUIT
-               deriving Show
+               deriving (Show, Eq)
 
 type ReplyCode = Int
 
@@ -107,49 +108,77 @@ codeToResponse 554 = TransactionFailed
 
 crlf = "\r\n"
 
-connectSMTPPort :: Integral a => String -> a -> IO Connect
-connectSMTPPort hostname port =
-    do sock <- socket AF_INET Stream 6
-       host <- inet_addr hostname `catch`
-               (\_ -> getHostByName hostname >>= return . hostAddress)
-       connect sock (SockAddrInet (fromInteger $ toInteger port) host)
-       (code, msg) <- parseResponse sock
-       unless (code == 220) $
-              do sClose sock
-                 fail "cannot connect to server"
-       senderHost <- getHostName
-       (code, msg) <- sendCommand (Connection sock []) (EHLO senderHost)
-       unless (code == 250) $
-              do (code, msg) <- sendCommand (Connection sock [])
-                                  (HELO senderHost)
-                 unless (code == 250) $
-                        do sClose sock
-                           fail "cannot connect to server"
-       return (Connection sock (tail $ lines msg))
+linesLF ""     = []
+linesLF "\r\n" = []
+linesLF s      = let (l, s') = break (=='\r') s
+                 in case s' of
+                      [] -> [l]
+                      '\r':'\n':s'' -> l : linesLF s''
+                      lf:s''        -> case linesLF s'' of
+                                         []    -> [l++"\r"]
+                                         hd:tl -> (l++"\r"++hd) : tl
+unlinesLF = concat . intersperse crlf
 
-connectSMTP :: String -> IO Connect
+isRight :: Either a b -> Bool
+isRight (Right _) = True
+isRight _         = False
+
+-- | connecting SMTP server with the specified name and port number.
+connectSMTPPort :: String  -- ^ name of the server
+                -> Int     -- ^ port number
+                -> IO SMTPConnection
+connectSMTPPort hostname port =
+    do conn <- openTCPPort hostname port
+       (code, msg) <- parseResponse conn
+       unless (code == 220) $
+              do close conn
+                 fail "cannot connect to the server"
+       senderHost <- getHostName
+       (code, msg) <- sendCommand (SMTPC conn []) (EHLO senderHost) 
+       unless (code == 250) $
+              do (code, msg) <- sendCommand (SMTPC conn []) (HELO senderHost)
+                 unless (code == 250) $
+                        do close conn
+                           fail "cannot connect to the server"
+       return (SMTPC conn (tail $ lines msg))
+
+-- | connecting SMTP server with the specified name and port 25.
+connectSMTP :: String     -- ^ name of the server
+            -> IO SMTPConnection
 connectSMTP = flip connectSMTPPort 25
 
-sendMessage :: Connect -> SMTPMethods -> IO (SMTPResponse, String)
-sendMessage sock (DATA dat) =
-    do ssize <- send sock $ "DATA\r\n"
-       unless (ssize == 6) $ fail "cannot send method DATA"
-       (code, msg) <- parseResponse sock
+parseResponse :: Connection -> IO (ReplyCode, String)
+parseResponse conn = do lst <- readLines
+                        return (read $ fst $ last lst, unlines $ map snd lst)
+    where readLines =
+              do l <- readLine conn
+                 unless (isRight l) $
+                        fail "cannot receive the server's response"
+                 case span (flip notElem " -") (either (\_ -> "") id l) of
+                   (code, '-':msg) -> fmap ((code, msg):) $ readLines
+                   (code, ' ':msg) -> return [(code, msg)]
+
+
+-- | send a method to a server
+sendCommand :: SMTPConnection -> Command -> IO (ReplyCode, String)
+sendCommand (SMTPC conn _) (DATA dat) =
+    do resp <- writeBlock conn $ "DATA\r\n"
+       unless (isRight resp) $ fail "cannot send method DATA"
+       (code, msg) <- parseResponse conn
        unless (code == 354) $ fail "this server cannot accept any data."
        mapM_ sendLine $ lines dat ++ ["."]
-       parseResponse sock
-    where sendLine l = do ssize <- send sock (l ++ crlf)
-                          unless (ssize == length l + 2) $
-                                 fail "cannot send data."
-sendCommand (Connection sock _) (AUTH PLAIN_AUTH username password) =
-    do ssize <- send sock command
-       unless (ssize == length command) $ fail "cannot send data."
-       parseResponse sock
-    where command = "AUTH PLAIN " ++ b64Encode (concat $ intersperce "\0" [username, username, password])
-sendCommand (Connection sock _) meth =
-    do ssize <- send sock command
-       unless (ssize == length command) $ fail "cannot send data."
-       parseResponse sock
+       parseResponse conn
+    where sendLine l = do resp <- writeBlock conn (l ++ crlf)
+                          unless (isRight resp) $ fail "cannot send data."
+sendCommand (SMTPC conn _) (AUTH PLAIN_AUTH username password) =
+    do resp <- writeBlock conn command
+       unless (isRight resp) $ fail "cannot send data."
+       parseResponse conn
+    where command = "AUTH PLAIN " ++ b64Encode (concat $ intersperse "\0" [username, username, password])
+sendCommand (SMTPC conn _) meth =
+    do resp <- writeBlock conn command
+       unless (isRight resp) $ fail "cannot send data."
+       parseResponse conn
     where command = case meth of
                       (HELO param) -> "HELO " ++ param ++ crlf
                       (EHLO param) -> "EHLO " ++ param ++ crlf
@@ -164,9 +193,13 @@ sendCommand (Connection sock _) meth =
                       RSET         -> "RSET\r\n"
                       QUIT          -> "QUIT\r\n"
 
-closeSMTP :: Connect -> IO ()
-closeSMTP conn = do sendMessage conn QUIT
-                    sClose conn
+-- | 
+-- close the connection.  This function send the QUIT method, so you
+-- do not have to QUIT method explicitly.
+closeSMTP :: SMTPConnection -> IO ()
+closeSMTP c@(SMTPC conn _) = do sendCommand c QUIT
+                                close conn
+
 
 -- | 
 -- sending a mail to a server. This is achieved by sendMessage.  If
@@ -174,7 +207,7 @@ closeSMTP conn = do sendMessage conn QUIT
 sendMail :: String   -- ^ sender mail
          -> [String] -- ^ receivers
          -> String   -- ^ data
-         -> Connect
+         -> SMTPConnection
          -> IO ()
 sendMail sender receivers dat conn =
     catcher `handle` mainProc
@@ -189,9 +222,12 @@ sendMail sender receivers dat conn =
 -- | 
 -- doSMTPPort open a connection, and do an IO action with the
 -- connection, and then close it.
-doSMTPPort :: Integral a => String -> a -> (Connect -> IO b) -> IO b
+doSMTPPort :: String -> Int -> (SMTPConnection -> IO a) -> IO a
 doSMTPPort host port execution =
     bracket (connectSMTPPort host port) closeSMTP execution
 
-doSMTP :: String -> (Connect -> IO a) -> IO a
+-- | 
+-- doSMTP is the similar to doSMTPPort, except that it does not
+-- require port number but connects to the server with port 25.
+doSMTP :: String -> (SMTPConnection -> IO a) -> IO a
 doSMTP host execution = doSMTPPort host 25 execution
