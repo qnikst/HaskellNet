2635e0933d1af877e8788c56ab63d930ef935400
mukai <mukai@jmuk.org>
2006-08-20 18:49:25 -0700

    bsGetContents now using hWaitForInput

--- example/hsgnutls.hs
+++ example/hsgnutls.hs
@@ -6,7 +6,7 @@ module TLSStream
     , connectTLSPort
     , TlsSession
     , sess
-    , fromSession )
+    )
     where
 
 import Network.GnuTLS
@@ -26,29 +26,32 @@ import Foreign.ForeignPtr
 import Foreign.Ptr
 
 data TlsSession t = TlsSession { sess :: Session t
+                               , hdl  :: Handle
                                , buf  :: IORef ByteString }
 
-fromSession :: Session t -> IO (TlsSession t)
-fromSession s = do newbuf <- newIORef BS.empty
-                   return $ TlsSession s newbuf
+fromSession :: Handle -> Session t -> IO (TlsSession t)
+fromSession h s = do newbuf <- newIORef BS.empty
+                     return $ TlsSession s h newbuf
 
 connectTLS :: HostName -> PortNumber -> IO (TlsSession Client)
 connectTLS host port = connectTLSPort host (PortNumber port)
 
 connectTLSPort :: HostName -> PortID -> IO (TlsSession Client)
 connectTLSPort host port =
-    do s <- tlsClient [ handle :=> connectTo host port
+    do h <- connectTo host port
+       s <- tlsClient [ handle :=> return h
                       , priorities := [CrtX509, CrtOpenpgp]]
        cred <- certificateCredentials
        set s [ credentials := cred]
        handshake s
-       fromSession s
+       fromSession h s
 
 
 
 bufLen = 4096
+waiting = 500 -- miliseconds
 
-extendBuf sess@(TlsSession s buf) =
+extendBuf sess@(TlsSession s _ buf) =
     do res <- mallocForeignPtrBytes bufLen
        len <- withForeignPtr res (\p -> tlsRecv s p bufLen)
        modifyIORef buf (flip BS.append $ BSB.fromForeignPtr res len)
@@ -61,7 +64,7 @@ doWhile cond execute =
 
 instance BSStream (TlsSession t) where
 #if defined(__GLASGOW_HASKELL__)
-    bsGetLine sess@(TlsSession s buf) =
+    bsGetLine sess@(TlsSession s _ buf) =
         do doWhile (readIORef buf >>= return . BS.notElem '\n')
                        (extendBuf sess)
            bufstr' <- readIORef buf
@@ -69,25 +72,32 @@ instance BSStream (TlsSession t) where
            writeIORef buf $ BS.tail rest
            return line
     bsGetLines sess = fmap BS.lines $ bsGetContents sess
-    bsGetNonBlocking = bsGet
+    bsGetNonBlocking sess@(TlsSession s h buf) len =
+        do doWhile (do r <- hWaitForInput h waiting
+                       l <- fmap BS.length $ readIORef buf
+                       return (r && l < len))
+                   (extendBuf sess)
+           bufstr' <- readIORef buf
+           let (r, bufstr'') = BS.splitAt len bufstr'
+           writeIORef buf bufstr''
+           return r
 #endif
-    bsGetContents sess@(TlsSession s buf) =
-        do doWhile (extendBuf sess >>= return . (==bufLen))
-                   (return ())
+    bsGetContents sess@(TlsSession s h buf) =
+        do doWhile (hWaitForInput h waiting) (extendBuf sess)
            bufstr <- readIORef buf
            writeIORef buf BS.empty
            return bufstr
-    bsGet sess@(TlsSession s buf) len =
+    bsGet sess@(TlsSession s _ buf) len =
         do doWhile (readIORef buf >>= return . (<len) . BS.length)
                    (extendBuf sess)
            bufstr' <- readIORef buf
            let (r, bufstr'') = BS.splitAt len bufstr'
            writeIORef buf bufstr''
            return r
-    bsPut (TlsSession s _) bs =
+    bsPut (TlsSession s h _) bs =
         do withForeignPtr fptr $ \ptr -> (tlsSend s (plusPtr ptr off) len)
            return ()
         where (fptr, off, len) = BSB.toForeignPtr bs
-    bsClose (TlsSession s _) = bye s ShutRdwr
+    bsClose (TlsSession s _ _) = bye s ShutRdwr
 
 
