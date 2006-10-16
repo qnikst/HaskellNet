1ba23f31cc959a2200fe11e152fc8679fb057001
mukai <mukai@jmuk.org>
2006-10-16 00:00:08 -0700

    JSON encoding estimater and converter is now available

--- Text/JSON.hs
+++ Text/JSON.hs
@@ -15,9 +15,17 @@
 -- 
 
 module Text.JSON
-    ( Jsonable(..)
+    ( -- * Basic Type and Type Class
+      Jsonable(..)
     , JsonNode(..)
-    , parse, parse', toDoc, toDocPP, pp)
+      -- * conversion Between String and Jsonable
+    , parse, parse'
+    , toDoc, toDocPP, pp, toDocRaw, toDocPPRaw
+    -- * converters and encode checkers
+    , toJsonString
+    , encoder
+    , utf8Conv, utf16BEConv, utf16LEConv, utf32BEConv, utf32LEConv
+    )
 where
 
 import Control.Monad hiding (join)
@@ -28,6 +36,7 @@ import Data.Char (ord, chr, isControl)
 import Data.List (intersperse)
 import qualified Data.Map as M
 import Data.Bits ((.|.), (.&.), shiftL, shiftR)
+import Data.Word (Word8)
 import Numeric (readHex, showHex)
 import Text.PrettyPrint.HughesPJ hiding (char)
 import Data.ByteString.Char8 (ByteString, pack, unpack)
@@ -222,43 +231,145 @@ parse' s = case dvNode (derive (Pos "JSON parser" 1 1) s) of
 
 
 toDoc :: JsonNode -> Doc
-toDoc = stringifyDoc hsep
+toDoc = stringifyDoc hsep True
 
-toDocPP :: Jsonable a => a -> Doc
-toDocPP = stringifyDoc fsep . toJson
+toDocPP :: JsonNode -> Doc
+toDocPP = stringifyDoc fsep True
 
 pp :: Jsonable a => a -> String
-pp = show . toDocPP
+pp = show . toDocPP . toJson
 
-stringifyDoc _ (String s) = stringifyString s
-stringifyDoc _ (Number x)
+toDocRaw :: JsonNode -> Doc
+toDocRaw = stringifyDoc hsep False
+toDocPPRaw :: JsonNode -> Doc
+toDocPPRaw = stringifyDoc fsep False
+
+
+stringifyDoc _ f (String s) = stringifyString s f
+stringifyDoc _ _ (Number x)
     | isInfinite x = error "can't stringify infinity"
     | isNaN x      = error "can't stringify NaN"
     | otherwise    = double x
-stringifyDoc sep (Object m) = lbrace <+> join comma [sep [stringifyString k <> colon, nest 2 (stringifyDoc sep v)] | (k,v) <- M.toList m] $$ rbrace
-stringifyDoc sep (Array xs) = lbrack <+> join comma (map (stringifyDoc sep) xs) <+> rbrack
-stringifyDoc _ (Bool b)   = if b then text "true" else text "false"
-stringifyDoc _ Null       = text "null"
+stringifyDoc sep f (Object m) = lbrace <+> join comma [sep [stringifyString k f <> colon, nest 2 (stringifyDoc sep f v)] | (k,v) <- M.toList m] $$ rbrace
+stringifyDoc sep f (Array xs) = lbrack <+> join comma (map (stringifyDoc sep f) xs) <+> rbrack
+stringifyDoc _ _ (Bool b)   = if b then text "true" else text "false"
+stringifyDoc _ _ Null       = text "null"
 
-stringifyString :: String -> Doc
-stringifyString s = doubleQuotes $ text $ concatMap f s
-    where f '"'  = "\\\""
-          f '\\' = "\\\\"
-          f '\b' = "\\b"
-          f '\f' = "\\f"
-          f '\n' = "\\n"
-          f '\r' = "\\r"
-          f '\t' = "\\t"
-          f '\DEL' = "\\u007f"
-          f c | c' > 0xffff = decSurrogates $ ord c
-              | c' > 0x00ff = printf "\\u%04x" c
-              | c' < 0x0020 = printf "\\u%04x" c
-              | otherwise   = [c]
+stringifyString :: String -> Bool -> Doc
+stringifyString s b = doubleQuotes $ text $ concatMap (f b) s
+    where f _ '"'  = "\\\""
+          f _ '\\' = "\\\\"
+          f _ '\b' = "\\b"
+          f _ '\f' = "\\f"
+          f _ '\n' = "\\n"
+          f _ '\r' = "\\r"
+          f _ '\t' = "\\t"
+          f _ '\DEL' = "\\u007f"
+          f True c = charToUTFChars c
+          f _ c | c' > 0xffff = decSurrogates $ ord c
+                | c' > 0x00ff = printf "\\u%04x" c
+                | c' < 0x0020 = printf "\\u%04x" c
+                | otherwise   = [c]
+                where c' = ord c
+                      decSurrogates c =
+                           let high = (c - 0x10000) `shiftR` 10 .|. 0xd800
+                               low  = c .&. 0x3ff .|. 0xdc00
+                           in "\\u" ++ showHex high ("\\u" ++ showHex low "")
+          charToUTFChars c
+              | c' < 0x7f      = [c]
+              | c' < 0x7ff     = chars 2
+              | c' < 0xffff    = chars 3
+              | c' < 0x1fffff  = chars 4
+              | c' < 0x3ffffff = chars 5
+              | otherwise      = chars 6
               where c' = ord c
-          decSurrogates c =
-              let high = (c - 0x10000) `shiftR` 10 .|. 0xd800
-                  low  = c .&. 0x3ff .|. 0xdc00
-              in "\\u" ++ showHex high ("\\u" ++ showHex low "")
+                    shifts = [30, 24, 18, 12, 6, 0]
+                    headMasks = [0xc0, 0xe0, 0xf0, 0xf8, 0xfc]
+                    maskShift s = 0x80 .|.  ((c' `shiftR` s) .&. 0x3f)
+                    chars n =
+                        let (h:t) = map maskShift $ drop (6-n) shifts
+                        in map chr ((h .|. (headMasks !! (n-2))) : t)
+
 
 join :: Doc -> [Doc] -> Doc
 join s = fcat . punctuate s
+
+
+----------------------------------------------------------------------
+-- string converter
+-- 
+
+toJsonString :: [Word8] -> String
+toJsonString s = (encoder s) s
+
+encoder :: [Word8] -> ([Word8] -> String)
+encoder (0:0:0:_:_) = utf32BEConv
+encoder (_:0:0:0:_) = utf32LEConv
+encoder (0:_:0:_:_) = utf16BEConv
+encoder (_:0:_:0:_) = utf16LEConv
+encoder _ = utf8Conv
+
+utf8Conv :: [Word8] -> String
+utf8Conv [] = []
+utf8Conv (c:cs)
+    | c < 0xc0 = chr (fromIntegral c) : utf8Conv cs
+    | c < 0xe0 = conv 1
+    | c < 0xf0 = conv 2
+    | c < 0xf8 = conv 3
+    | c < 0xfc = conv 4
+    | otherwise = conv 5
+    where conv n = let (hd, tl) = splitAt n cs
+                       c' = fromIntegral c
+                   in utf8CharsToChar n c' (map fromIntegral hd) : utf8Conv tl
+
+utf8CharsToChar :: Int -> Int -> [Int] -> Char
+utf8CharsToChar n c cs =
+    chr $ foldl f ((c .&. mask) `shiftL` hs) $ zip shifts $ map (.&. 0x3f) cs
+    where (hs:shifts) = drop (5-n) [30,24,18,12,6,0]
+          mask = [0x1f,0x0f, 0x07, 0x03, 0x01] !! n
+          f c' (s, c) = c' .|. (c `shiftL` s)
+
+utf16Conv :: (Int -> Int -> Int) -> [Word8] -> String
+utf16Conv _ [] = ""
+utf16Conv _ [_] = ""
+utf16Conv f (c1:c2:cs) =
+    let c = f (fromIntegral c1) (fromIntegral c2)
+    in if (c .&. 0xd800) == 0xd800
+       then surrogatePair c cs
+       else chr (c + 0x10000) : utf16Conv f cs
+    where surrogatePair c (c1:c2:cs) =
+              if (c' .&. 0xdc00) == 0xdc00
+              then chr (high .|. low) : utf16Conv f cs
+              else error "invalid UTF-16 encoding"
+              where c' = f (fromIntegral c1) (fromIntegral c2)
+                    high = (c .&. 0x03ff) `shiftL` 10
+                    low  = c' .&. 0x03ff
+          surrogatePair c _ = error "invalid UTF-16 encoding"
+
+          nextChar (c1:c2:cs) =
+              (f (fromIntegral c1) (fromIntegral c2), cs)
+
+utf16LEConv :: [Word8] -> String
+utf16LEConv = utf16Conv f
+    where f c1 c2 = c2 `shiftL` 8 .|. c1
+
+utf16BEConv :: [Word8] -> String
+utf16BEConv = utf16Conv f
+    where f c1 c2 = c1 `shiftL` 8 .|. c2
+
+utf32Conv :: (Int -> Int -> Int -> Int -> Int) -> [Word8] -> String
+utf32Conv f (c1:c2:c3:c4:cs) =
+    chr (f (fromIntegral c1) (fromIntegral c2) (fromIntegral c3) (fromIntegral c4)) : utf32Conv f cs
+utf32Conv _ _ = ""
+
+utf32BEConv :: [Word8] -> String
+utf32BEConv = utf32Conv f
+    where f c1 c2 c3 c4 =
+              foldl1 (.|.) $ zipWith (\c s -> shiftL c s) [c1,c2,c3,c4] shifts
+          shifts = [24, 16, 8, 0]
+
+utf32LEConv :: [Word8] -> String
+utf32LEConv = utf32Conv f
+    where f c1 c2 c3 c4 =
+              foldl1 (.|.) $ zipWith (\c s -> shiftL c s) [c1,c2,c3,c4] shifts
+          shifts = [0, 8, 16, 24]
