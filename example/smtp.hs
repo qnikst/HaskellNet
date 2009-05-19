6628cc1274863b7d0cb362bfca33c37165b5a2f6
wrwills <wrwills@gmail.com>
2009-05-19 08:16:18 -0700

    added installation info

--- example/smtp.hs
+++ example/smtp.hs
@@ -5,11 +5,11 @@ import qualified Data.ByteString.Char8 as BS
 
 smtpServer = "outmail.f2s.com"
 sendFrom = "test@test.org"
-sendTo = [""]
+sendTo = ["wrwills@gmail.com"]
 
 main = do
   con <- connectSMTP smtpServer
   let msg = ([("From", "HaskellNet <" ++ sendFrom ++ ">"),("Subject","Test")], BS.pack "\r\nhello from haskellnet")
-  sendMail "robert.wills@dicta.org.uk" ["wrwills@gmail.com"] (BS.pack $ show $ showMessage "utf-8" msg) con
+  sendMail sendFrom sendTo (BS.pack $ show $ showMessage "utf-8" msg) con
   closeSMTP con
          
