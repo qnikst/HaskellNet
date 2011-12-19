60ff9dc3993bbcb0a6584da54a36870a61b69a68
Jonathan Daugherty <jtd@galois.com>
2011-12-18 16:45:35 -0800

    example/imap: warnings, fixes

--- example/imap.hs
+++ example/imap.hs
@@ -1,20 +1,18 @@
 import System.IO
 import Network.HaskellNet.IMAP
-import Text.Mime
 import qualified Data.ByteString.Char8 as BS
 import Control.Monad
 
 imapServer = "imap.mail.org"
-user = ""
-pass = ""
+username = ""
+password = ""
 
 main = do
   con <- connectIMAP imapServer
-  login con user pass
+  login con username password
   mboxes <- list con
   mapM print mboxes
   select con "INBOX"
   msgs <- search con [ALLs]
   mapM_ (\x -> print x) (take 4 msgs)
   forM_ (take 4msgs) (\x -> fetch con x >>= print)
-         
