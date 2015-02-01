9b49e408a9d7d2837e72eb16324b5eec8aae8cce
Leza Morais Lutonda (Lemol-C) <lemol-c@hotmail.com>
2015-02-01 06:31:51 -0500

    Update in Debug.hs

--- src/Network/HaskellNet/Debug.hs
+++ src/Network/HaskellNet/Debug.hs
@@ -9,14 +9,14 @@ import qualified Data.ByteString.Char8 as BS
 
 import System.IO
 
-debugStream :: BSStream -> BSStream
+debugStream :: BSStream IO -> BSStream IO
 debugStream inner =
     inner { bsGetLine = debugBsGetLine inner
           , bsGet = debugBsGet inner
           , bsPut = debugBsPut inner
           }
 
-debugBsGetLine :: BSStream -> IO BS.ByteString
+debugBsGetLine :: BSStream IO -> IO BS.ByteString
 debugBsGetLine s = do
   hPutStr stderr "reading with bsGetLine..."
   hFlush stderr
@@ -24,7 +24,7 @@ debugBsGetLine s = do
   BS.hPutStrLn stderr l
   return l
 
-debugBsGet :: BSStream -> Int -> IO BS.ByteString
+debugBsGet :: BSStream IO -> Int -> IO BS.ByteString
 debugBsGet s len = do
   hPutStr stderr $ "reading with bsGet "++show len++"..."
   hFlush stderr
@@ -32,11 +32,11 @@ debugBsGet s len = do
   BS.hPutStrLn stderr chunk
   return chunk
 
-debugBsPut :: BSStream -> BS.ByteString -> IO ()
+debugBsPut :: BSStream IO -> BS.ByteString -> IO ()
 debugBsPut s str = do
   hPutStr stderr "putting with bsPut ("
   BS.hPutStrLn stderr str
-  hPutStr stderr (")...")
+  hPutStr stderr ")..."
   hFlush stderr
   bsPut s str
   bsFlush s
