7b716502bf6415f4d5c53f56d070862632259774
Alexander Vershilov <alexander.vershilov@sirius.online>
2020-12-23 01:43:25 +0300

    Add documentation about SMTPConnection internals.

--- src/Network/HaskellNet/SMTP/Internal.hs
+++ src/Network/HaskellNet/SMTP/Internal.hs
@@ -6,6 +6,7 @@ module Network.HaskellNet.SMTP.Internal
   ( SMTPConnection(..)
   , Command(..)
   , Response(..)
+  , ReplyCode
   , tryCommand
   , parseResponse
   , sendCommand
@@ -23,11 +24,13 @@ import Network.HaskellNet.BSStream
 
 import Prelude
 
-
 -- | All communication with server is done using @SMTPConnection@ value.
-data SMTPConnection = SMTPC { bsstream :: !BSStream, _response :: ![ByteString] }
--- XXX: The response field seems to be unused. It's saved at one place, but never
--- retrieved.
+data SMTPConnection = SMTPC {
+  -- | Connection communication channel.
+  bsstream :: !BSStream,
+  -- | Server properties as per reply to the 'EHLO' request.
+  _response :: ![ByteString]
+  }
 
 -- | SMT commands.
 --
@@ -46,6 +49,7 @@ data Command = HELO String
              | QUIT
                deriving (Show, Eq)
 
+-- | Code reply from the server. It's always 3 digit integer.
 type ReplyCode = Int
 
 -- | Possible server response.
@@ -73,8 +77,28 @@ data Response = Ok
               | TransactionFailed
                 deriving (Show, Eq)
 
-tryCommand :: SMTPConnection -> Command -> Int -> [ReplyCode]
-           -> IO ByteString
+-- | Safe wrapper for running a client command over the SMTP
+-- connection.
+--
+-- /Note on current behavior/
+--
+-- We allow the command to fail several times, retry
+-- happens in case if we have received unexpected status code.
+-- In this case message will be sent again. However in case
+-- of other synchronous or asynchronous exceptions there will
+-- be no retries.
+--
+-- It case if number of retries were exceeded connection will
+-- be closed automatically.
+--
+-- The behaviors in notes will likely be changed in the future
+-- and should not be relied upon, see issues #76, #77.
+tryCommand
+  :: SMTPConnection -- ^ Connection
+  -> Command -- ^ Supported command
+  -> Int -- ^ Number of allowed retries
+  -> [ReplyCode] -- ^ List of accepted codes
+  -> IO ByteString -- ^ Resulting data
 tryCommand conn cmd tries expectedReplies = do
     (code, msg) <- sendCommand conn cmd
     case () of
@@ -95,6 +119,30 @@ tryCommand conn cmd tries expectedReplies = do
     prettyExpected [x] = "expected reply code of " ++ show x
     prettyExpected xs = "expected any reply code of " ++ show xs
 
+-- | Read response from the stream. Response consists of the code
+-- and one or more lines of data.
+--
+-- In case if it's not the last line of reply the code is followed
+-- by the '-' sign. We return the code and all the data with the code
+-- stripped.
+--
+-- Eg.:
+--
+-- @
+-- "250-8BITMIME\r"
+-- "250-PIPELINING\r"
+-- "250-SIZE 42991616\r"
+-- "250-AUTH LOGIN PLAIN XOAUTH2\r"
+-- "250-DSN\r"
+-- "250 ENHANCEDSTATUSCODES\r"
+-- @
+--
+-- Returns:
+--
+-- @
+-- (250, "8BITMIME\\nPIPELINING\nSIZE 42991616\\nAUTH LOGIN PLAIN XOAUTH2\\nDSN\\nENHANCEDSTATUSCODES")
+-- @
+--
 parseResponse :: BSStream -> IO (ReplyCode, ByteString)
 parseResponse st =
     do (code, bdy) <- readLines
@@ -107,8 +155,12 @@ parseResponse st =
                             return (c2, BS.tail bdy:ls)
                     else return (c, [BS.tail bdy])
 
--- | send a method to a server
-sendCommand :: SMTPConnection -> Command -> IO (ReplyCode, ByteString)
+-- | Sends a 'Command' to the server. Function that performs all the logic
+-- for sending messages. Throws an exception if something goes wrong.
+sendCommand
+  :: SMTPConnection
+  -> Command
+  -> IO (ReplyCode, ByteString)
 sendCommand (SMTPC conn _) (DATA dat) =
     do bsPutCrLf conn $ BS.pack "DATA"
        (code, msg) <- parseResponse conn
@@ -156,13 +208,13 @@ sendCommand (SMTPC conn _) meth =
                       (AUTH {})     ->
                           error "BUG: AUTH pattern should be matched by sendCommand patterns"
 
--- | close the connection.  This function send the QUIT method, so you
+-- | Closes the connection.  This function send the QUIT method, so you
 -- do not have to QUIT method explicitly.
 closeSMTP :: SMTPConnection -> IO ()
 closeSMTP (SMTPC conn _) = bsClose conn
 
--- | sending a mail to a server. This is achieved by 'sendMessage'. If
--- something is wrong, it raises an 'IOException'.
+-- | Sends a mail to the server. This is achieved by the 'tryCommand'.
+-- If something is wrong, it raises an 'IOException'.
 sendMail :: String     -- ^ sender mail
          -> [String]   -- ^ receivers
          -> ByteString -- ^ data
@@ -177,8 +229,10 @@ sendMail sender receivers dat conn = do
     -- Try the command once and @fail@ if the response isn't 250.
     sendAndCheck cmd = tryCommand conn cmd 1 [250, 251]
 
+-- | Just a crlf constant.
 crlf :: BS.ByteString
 crlf = BS.pack "\r\n"
 
+-- | Write a message ending with ctlf.
 bsPutCrLf :: BSStream -> ByteString -> IO ()
 bsPutCrLf h s = bsPut h (s <> crlf)
